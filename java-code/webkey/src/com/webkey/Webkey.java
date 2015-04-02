f7059269e044d32e28b834f68534def7bc60d5fb
Leonard Koenig <leonard.r.koenig@googlemail.com>
2015-04-02 18:47:32 +0200

    added initial code

---
+++ java-code/webkey/src/com/webkey/Webkey.java
@@ -0,0 +1,52 @@
+package com.webkey;
+
+import com.webkey.R;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+
+public class Webkey extends TabActivity {
+	
+    /** Called when the activity is first created. */
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+
+	    Resources res = getResources(); // Resource object to get Drawables
+	    TabHost tabHost = getTabHost();  // The activity TabHost
+	    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+	    Intent intent;  // Reusable Intent for each tab
+
+	    
+	    // Create an Intent to launch an Activity for the tab (to be reused)
+	    intent = new Intent().setClass(this, com.webkey.activity.WebkeyTab.class);
+	    spec = tabHost.newTabSpec("control").setIndicator("Control",
+	                      res.getDrawable(R.drawable.ic_tab_control))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+	    
+	    intent = new Intent().setClass(this, com.webkey.activity.SettingsTab.class);
+	    spec = tabHost.newTabSpec("settings").setIndicator("Settings",
+	                      res.getDrawable(R.drawable.ic_tab_settings))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+	    
+
+	    intent = new Intent().setClass(this, com.webkey.activity.ChatUI.class);
+	    spec = tabHost.newTabSpec("chat").setIndicator("Chat",
+	                      res.getDrawable(R.drawable.ic_tab_chat))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+	
+	    tabHost.setCurrentTab(0);
+	}
+	
+	@Override
+	public void onResume(){		
+		super.onResume();
+	}		
+}
